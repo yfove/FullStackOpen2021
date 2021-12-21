a47f3b3d1460dcfaf5e8ba4bd3e98cb2ba142843
curtis chat <curtis.ch.chat@gmail.com>
2021-12-21 15:29:05 -0700

    2.2: Course informatipon step7 implement sum component

--- part2/coursecontents/src/components/Course.js
+++ part2/coursecontents/src/components/Course.js
@@ -30,13 +30,22 @@ const Content = ({ course }) => {
     )
 }
 
+const Total = ({ course }) => {
+    const sum = course.parts.reduce((total, part) => total + part.exercises, 0)
+    return (
+        <p>Total number of exercises <strong>{sum}</strong></p>
+    )
+}
+
 const Course = ({ course }) => {
     return (
         <>
             <Header course={course} />
             <Content course={course} />
+            <Total  course={course} />
         </>
     )
 }
 
 export default Course
+
