d0f082c361048f4f07db3c46a9e98b93aed831a2
curtis chat <curtis.ch.chat@gmail.com>
2021-12-23 13:30:47 -0700

    2.7: The Phonebook Step2 form validation for name duplicates

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,39 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('')
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setNewName("");
+    const alreadyExists = persons.some((person) => person.name === newName);
+    // some() tests if at least on element passes the test. if true finds element & true
+    if (alreadyExists) {
+      alert(`${newName} is already added to phonebook`)
+      return;
+    }
+    setPersons(persons.concat({name: newName}));
+  }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form>
-        <div>
-          name: <input />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-        <div>debug: {newName}</div>
-      </form>
-      <h2>Numbers</h2>
-      ...
-    </div>
-  )
-}
+    <h2>Phonebook</h2>
+    <form onSubmit={onFormSubmit}>
+      <div>
+        name:{" "}
+        <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+    <h2>Numbers</h2>
+    {persons.map((person) => (
+      <p key={person.name}>{person.name}</p>
+    ))}
+  </div>
+  );  
+};
 
 export default App
