20977f0a1cbbf93a051a157a07a707e3c25094fb
curtis chat <curtis.ch.chat@gmail.com>
2022-01-10 17:01:08 -0700

    resolve flag image not showing up

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -4,14 +4,14 @@ const Country = ({ data: { name, capital, population, flag, languages} }) => {
     return (
         <>
             <h1>{name}</h1>
-            <p>capital {capital}</p>
-            <p>population {population}</p>
+            <p>Capital: {capital}</p>
+            <p>Populatiion: {population}</p>
             <ul>
                 {languages.map((language) => (
                     <li>{language.name}</li>
                 ))}
             </ul>
-            <img src="{flag}" alt="{name}" width="100px" />
+            <img src={flag} alt={name} width="20%" />
         </>
     )
 }
